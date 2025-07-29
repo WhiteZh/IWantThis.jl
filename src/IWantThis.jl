aa72d71171877dd93363c5171da81278af364259
John Bonjour <whitewolfzhang@gmail.com>
2025-07-29 12:39:28 -0700

    everyone you learn something

--- src/IWantThis.jl
+++ src/IWantThis.jl
@@ -5,9 +5,9 @@ export \, Set
 import Base.\
 
 "Operator overload for `setdiff` only between Sets"
-\(A::Set{T}, B::Set{T})::Set{T} where T = setdiff(A, B)
+(\(A::Set{T}, B::Set{T})::Set{T}) where T = setdiff(A, B)
 
 "A varargs constructor for Set like the one of Dict"
-Set(elems::T...)::Set{T} where T = Set([ele for ele ∈ elems])
+(Set(elems::T...)::Set{T}) where T = Set([ele for ele ∈ elems])
 
 end # module IWantThis
