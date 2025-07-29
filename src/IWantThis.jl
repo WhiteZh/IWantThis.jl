9dd9b833929c4c8a7a2eb32ac065b68366fb7dbf
John Bonjour <whitewolfzhang@gmail.com>
2025-07-29 12:29:14 -0700

    first commit

---
+++ src/IWantThis.jl
@@ -0,0 +1,13 @@
+module IWantThis
+
+export \, Set
+
+import Base.\
+
+"Operator overload for `setdiff` only between Sets"
+\(A::Set{T}, B::Set{T})::Set{T} where T = setdiff(A, B)
+
+"A varargs constructor for Set like the one of Dict"
+Set(elems::T...)::Set{T} where T = Set([ele for ele ∈ elems])
+
+end # module IWantThis
